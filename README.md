68cf32c8089aec4fa32ab118bce0b33009b0ccf4
Milena <132115087+milena-allaway@users.noreply.github.com>
2023-08-08 22:08:42 -0400

    finished script, updated html and css, added readme

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
-# agenda
-An agenda app to keep track of your daily schedule
+# Weekly Scheduler
+
+## Description
+An agenda app to keep track of your daily schedule. As an employee with a busy schedule, I want to add and save important events to a daily planner, so that I can manage my time effectivley. The day and time are displayed at the top and the day is separated into one hour blocks. The displayed time blocks are for standard business hours of 9am - 5pm. The day's event blocks are also color coded, indicating 'past, present, and future' time. 
+This project was a fun one, and I learned a lot about the dayjs library. Once again, my tutor, Katie Redford, provided guidance when I was stuck, and I was able to better understand how to loop over elements, isolate an id, and parse that data into a compareable variable, all using jquery. 
+
+## Installation
+N/A no installation required
+
+## Usage
+When you visit (insert url here) you will see a page that looks likes this:
+
+
+You then can input events into the text area within your prefered time block and click the save button to save them.
+If you refresh the page, the saved events will still display. The day will be broken down into color coded areas of 'past, present, and future' depending on the current time. 
+
+## Credits
+My tutor, Katie Redford, guided me through looping through elements and isolating their id, along with the following online resources:
+https://www.w3schools.com/jsref/jsref_split.asp
+https://www.tutorialrepublic.com/faq/how-to-get-the-id-of-an-element-using-jquery.php#:~:text=Answer%3A%20Use%20the%20jQuery%20attr,alert%20box%20on%20button%20click.
+
+The following online resources were used to assist with selecting the classes of elements and putting thier input values into an array:
+// https://www.w3schools.com/jquery/jquery_selectors.asp
+  // https://stackoverflow.com/questions/24738735/
+
+Information about the "for each" loop
+https://api.jquery.com/each/#each-function
+
+Information about Dayjs:
+https://day.js.org/docs/en/display/format
+
+Starter code provided by: https://github.com/coding-boot-camp/crispy-octo-meme
+
+All bootcamp course content relating to jquery, click events, local storage, and dayjs was also referenced in the making of this project.
+
+## License
+MIT License
+
+Copyright (c) 2023 Milena
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+
+
--- assets/script.js
+++ assets/script.js
@@ -1,34 +1,21 @@
-var body = $('body');
-var divContainer = $('.container-fluid');
-var nineAM = $('#hour-9');
-var tenAM = $('#hour-10');
-var elevenAM = $('#hour-11');
-var noon = $('#hour-12');
-var onePM = $('#hour-1');
-var twoPM = $('#hour-2');
-var threePM = $('#hour-3');
-var fourPM = $('#hour-4');
-var fivePM = $('#hour-5');
-var saveBtn = $('.btn');
-var description = $('.description');
-
-// test
-
-// var thingToDo9am = divContainer.children().eq(0).children().eq(1);
-// nineAM.children().eq(1).css("background-color", "blue");
-saveBtn.css("background-color", "hotpink");
-// thingToDo9am.text("hello");
-
-
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+//All console.logs are used for testing purposes
 //https://www.w3schools.com/jquery/event_ready.asp
 $(document).ready(function() {
-  
-  // TODO: Add a listener for click events on the save button. 
+  var body = $('body');
+  var saveBtn = $('.btn');
+  var description = $('.description'); //'To Dos'
+  //get just the hour of current time in 24 hour format
+  var time = dayjs();
+    console.log(time.format('h:mm a'));
+    hour = time.format('H');
+    console.log("hour is: " + hour);
+
+  saveBtn.css("background-color", "hotpink");
+  description.css("font-family", "monospace", "font-weight", "bold");
+
   // https://www.w3schools.com/jquery/jquery_selectors.asp
   // https://stackoverflow.com/questions/24738735/
+  //click event for save button to save text input to local storage
   $(saveBtn).click(function() {
     var toDo = []
     $("textarea.description").each(function () {
@@ -40,42 +27,38 @@ $(document).ready(function() {
     localStorage.setItem("ToDos", savedToDos)
   
   });
-
-
   
+  //tutor helped with ' parseInt($(this).attr("id").split("-")[1]);'
+  // https://www.w3schools.com/jsref/jsref_split.asp
+  // https://www.tutorialrepublic.com/faq/how-to-get-the-id-of-an-element-using-jquery.php#:~:text=Answer%3A%20Use%20the%20jQuery%20attr,alert%20box%20on%20button%20click.
+  //make color of time blocks change depending on current time
+  $(".time-block").each(function () {
+   console.log("timeblock: ", $(this).attr("id").split("-")[1]);
+   var timeblock = parseInt($(this).attr("id").split("-")[1]);
+   if (timeblock < hour ) {
+    $(this).addClass('past');
+   } else if (timeblock === hour) {
+    $(this).addClass('present')
+   } else {
+    $(this).addClass('future')
+   };
+   
+  });
 
-
-  // This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
+  //get and display text saved in local storage
   var getSavedToDos = localStorage.getItem("ToDos");
   var getToDo = JSON.parse(getSavedToDos);
   console.log(getToDo);
+  
   // https://api.jquery.com/each/#each-function
   $("textarea.description").each(function (i) {
     $(this).text(getToDo[i]);
+    console.log("this is: " + this);
   });
-  //
-  // TODO: Add code to display the current date in the header of the page.
+  
+  //display current date and time
   var today = dayjs();
   $('#currentDay').text(today.format('dddd, MMMM D YYYY, h:mm:ss a'));
   $(body).css("text-align", "center");
-  var time = $(date.now());
-  console.log(time);
- 
-
 
 });
