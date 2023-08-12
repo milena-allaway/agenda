926b91d8a5dc1a65edb03ca886b96ee23f622aa9
Milena <132115087+milena-allaway@users.noreply.github.com>
2023-08-11 21:17:53 -0400

    after another deep dive and checking every line of code, fixed defined 'null' error for empty local storage, fixed syntax for .description css, parseInt for hour variable and adjusted if statement accordingly from == to ===, previous == was a lazy fix, fixed syntax for some declared variables by removing the  were appropriate, added missing semi-colons for your viewing pleasure. triple checked everything

--- assets/script.js
+++ assets/script.js
@@ -7,16 +7,16 @@ $(document).ready(function() {
   //get just the hour of current time in 24 hour format
   var time = dayjs();
     console.log(time.format('h:mm a'));
-    hour = time.format('H');
+    hour = parseInt(time.format('H'));
     console.log("hour is: " + hour);
   
    //display current date and time
    var today = dayjs();
    $('#currentDay').text(today.format('dddd, MMMM D YYYY, h:mm:ss a'));
-   $(body).css("text-align", "center");  
+   body.css("text-align", "center");  
 
   saveBtn.css("background-color", "hotpink");
-  description.css("font-family", "monospace", "font-weight", "bold");
+  description.css({"font-family": "monospace", "font-weight": "bold"});
 
   //tutor helped with 'parseInt($(this).attr("id").split("-")[1]);'
   // https://www.w3schools.com/jsref/jsref_split.asp
@@ -27,10 +27,10 @@ $(document).ready(function() {
     var timeblock = parseInt($(this).attr("id").split("-")[1]);
     if (timeblock < hour ) {
      $(this).addClass('past');
-    } else if (timeblock == hour) {
-     $(this).addClass('present')
+    } else if (timeblock === hour) {
+     $(this).addClass('present');
     } else {
-     $(this).addClass('future')
+     $(this).addClass('future');
     };
     
    });
@@ -38,21 +38,22 @@ $(document).ready(function() {
   // https://www.w3schools.com/jquery/jquery_selectors.asp
   // https://stackoverflow.com/questions/24738735/
   //click event for save button to save text input to local storage
-  $(saveBtn).click(function() {
-    var toDo = []
+  saveBtn.click(function() {
+    var toDo = [];
     $("textarea.description").each(function () {
       toDo.push($(this).val());
     });
   
     var toDos = toDo;
     var savedToDos = JSON.stringify(toDos);
-    localStorage.setItem("ToDos", savedToDos)
+    localStorage.setItem("ToDos", savedToDos);
   
   });
-  
+  // used following site info to help solve issue on loading of empty local storage, getToDo defined null
+  //https://stackoverflow.com/questions/3262605/how-to-check-whether-a-storage-item-is-set
   //get and display text saved in local storage
   var getSavedToDos = localStorage.getItem("ToDos");
-  var getToDo = JSON.parse(getSavedToDos);
+  var getToDo = JSON.parse(getSavedToDos) || []; //if local storage is empty, getToDo array is defined as "[]"
   console.log(getToDo);
   
   // https://api.jquery.com/each/#each-function
